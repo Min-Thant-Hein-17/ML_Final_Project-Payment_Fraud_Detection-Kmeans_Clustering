e5a082816f3a8bf7e39980bc03905b708f1c5abf
minthanthein17 <minthanthein@parami.edu.mm>
2025-12-23 14:36:45 +0630

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(page_title="Fraud Sentinel", layout="wide")
 
 st.sidebar.title("🛡️ Project Alpha")
-st.sidebar.subtitle("Anti-Fraud Units")
+st.sidebar.subheader("Anti-Fraud Units")
 st.sidebar.image("https://talloiresnetwork.tufts.edu/wp-content/uploads//Parami-University-1.png", use_container_width=True)
 
 st.sidebar.markdown("---")
@@ -97,6 +97,7 @@ if st.button("✨ Identify Cluster", type="primary"):
         st.markdown(f"### Result: :{res['color']}[{res['name']} (Cluster {cluster})]")
         st.write(f"**Calculated Risk Level:** {res['risk']}")
         st.info(f"  {res['rec']}")
+
 
 
 
